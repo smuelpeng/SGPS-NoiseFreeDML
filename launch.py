97241ac932162a149590532f1a56b5f9699e27d5
yuzhipeng <1101260212@qq.com>
2025-01-21 13:07:27 +0800

    update

--- launch.py
+++ launch.py
@@ -3,6 +3,20 @@ import contextlib
 import logging
 import os
 import sys
+import subprocess
+import signal
+import requests
+import json
+import urllib.request
+import urllib.parse
+import io
+from websocket import WebSocket
+import uuid
+
+INITIAL_PORT = int(os.getenv('INITIAL_PORT')) if os.getenv('INITIAL_PORT') is not None else 7860
+API_COMMAND_LINE = os.getenv('API_COMMAND_LINE') if os.getenv('API_COMMAND_LINE') is not None else 'python /root/ComfyUI/main.py --highvram --fast '
+MAX_COMFY_START_ATTEMPTS = int(os.getenv('MAX_COMFY_START_ATTEMPTS')) if os.getenv('MAX_COMFY_START_ATTEMPTS') is not None else 10
+API_URL = os.getenv('API_URL') if os.getenv('API_URL') is not None else '127.0.0.1'
 
 class ColoredFilter(logging.Filter):
     """
@@ -37,6 +51,70 @@ class ColoredFilter(logging.Filter):
             record.msg = f"{record.msg}{self.RESET}"
         return True
 
+
+class FeatureServerInterface:
+    _instance = None
+    _process = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ModuleInterface, cls).__new__(cls)
+        return cls._instance
+
+    def find_available_port(self): # If the initial port is already in use, this method finds an available port to start the API server on
+        port = INITIAL_PORT
+        while True:
+            try:
+                response = requests.get(f'http://{API_URL}:{port}')
+                if response.status_code != 200:
+                    return port
+                else:
+                    port += 1
+            except requests.ConnectionError:
+                return port
+
+    def start_api(self): # This method is used to start the API server
+        if not self.is_api_running(): # Block execution until the API server is running
+            api_command_line = API_COMMAND_LINE + f" --port {self.urlport}" # Add the port to the command line
+            if self._process is None or self._process.poll() is not None: # Check if the process is not running or has terminated for some reason
+                self._process = subprocess.Popen(api_command_line.split())
+                print("API process started with PID:", self._process.pid)
+                attempts = 0
+                while not self.is_api_running(): # Block execution until the API server is running
+                    if attempts >= MAX_COMFY_START_ATTEMPTS:
+                        raise RuntimeError(f"API startup procedure failed after {attempts} attempts.")
+                    time.sleep(COMFY_START_ATTEMPTS_SLEEP)  # Wait before checking again, for 1 second by default
+                    attempts += 1 # Increment the number of attempts
+                print(f"API startup procedure finalized after {attempts} attempts with PID {self._process.pid} in port {self.urlport}")
+                time.sleep(1.5)  # Wait for 0.5 seconds before returning
+
+    def is_api_running(self): # This method is used to check if the API server is running
+        test_payload = TEST_PAYLOAD
+        try:
+            print(f"Checking web server is running in {self.server_address}...")
+            response = requests.get(self.server_address)
+            if response.status_code == 200: # Check if the API server tells us it's running by returning a 200 status code
+                self.ws.connect(self.ws_address)
+                print(f"Web server is running (status code 200). Now trying test image...")
+
+                # TODO  # Check if the API server is actually running by sending a test request.
+
+                if test_image is not None:  # this ensures that the API server is actually running and not just the web server
+                    return True
+                return False
+        except Exception as e:
+            print("API not running:", e)
+            return False
+
+
+
+
+
+
+
+
+
+
 def main(args, extras) -> None:
     # set CUDA_VISIBLE_DEVICES if needed, then import pytorch-lightning
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -62,7 +140,6 @@ def main(args, extras) -> None:
     from pytorch_lightning.utilities.rank_zero import rank_zero_only
     if args.typecheck:
         from jaxtyping import install_import_hook
-
         install_import_hook("sgps", "typeguard.typechecked")
         
     import sgps
@@ -181,15 +258,6 @@ def main(args, extras) -> None:
         # manually set epoch and global_step as they cannot be automatically resumed
         set_system_status(system, cfg.resume)
         trainer.validate(system, datamodule=dm, ckpt_path=cfg.resume)
-    # elif args.test:
-    #     # manually set epoch and global_step as they cannot be automatically resumed
-    #     set_system_status(system, cfg.resume)
-    #     trainer.test(system, datamodule=dm, ckpt_path=cfg.resume)
-    # elif args.export:
-    #     set_system_status(system, cfg.resume)
-    #     trainer.predict(system, datamodule=dm, ckpt_path=cfg.resume)
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -208,7 +276,6 @@ if __name__ == "__main__":
     group.add_argument("--validate", action="store_true")
     group.add_argument("--test", action="store_true")
     group.add_argument("--export", action="store_true")
-
     parser.add_argument("--wandb", action="store_true", help="if true, log to wandb")
 
     parser.add_argument(
@@ -239,11 +306,3 @@ if __name__ == "__main__":
             main(args, extras)
     else:
         main(args, extras)
-
-
-
-
-
-
-
-
